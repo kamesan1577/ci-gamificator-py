e11b4a80ce2483f934fd5158c31c433335ff108c
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-07-03 16:02:04 +0900

    first commit

---
+++ app/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name)
+    db.add(db_user)
+    db.commit()
+    db.reflesh(db_user)
+    return db_user
